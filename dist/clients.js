7461e61abb8d7558b512f301e11b91c7716d699c
Jhosmar <jhosmardsuarezc961@gmail.com>
2025-02-19 12:34:24 -0400

    implement client management functions with async support and add morgan for logging

--- dist/clients.js
+++ dist/clients.js
@@ -1,18 +1,62 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createClient = createClient;
+exports.deleteClient = deleteClient;
+exports.getClients = getClients;
+exports.updateClients = updateClients;
 let clients = [];
 function clientExists(cedula) {
-    return clients.some(client => client.id === cedula);
+    return __awaiter(this, void 0, void 0, function* () {
+        return clients.some(client => client.id === cedula);
+    });
+}
+function getClients() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return clients;
+    });
 }
 function createClient(cedula, nombre) {
-    if (!clientExists(cedula)) {
-        let client = {
-            id: cedula,
-            name: nombre
-        };
-        clients.push(client);
-        return { success: true, clients: clients };
-    }
-    return { success: false, message: "Client already exists" };
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!(yield clientExists(cedula))) {
+            let client = {
+                id: cedula,
+                name: nombre
+            };
+            clients.push(client);
+            return { success: true, clients: clients };
+        }
+        return { success: false, message: "Client already exists" };
+    });
+}
+function deleteClient(cedula) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (yield clientExists(cedula)) {
+            clients = clients.filter(client => client.id !== cedula);
+            return { success: true, clients: clients };
+        }
+        return { success: false, message: "Client doesn't exist." };
+    });
+}
+function updateClients(cedula, nombre) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (yield clientExists(cedula)) {
+            clients = clients.map(client => {
+                if (client.id === cedula) {
+                    client.name = nombre;
+                }
+                return client;
+            });
+            return { success: true, clients: clients };
+        }
+        return { success: false, message: "Client doesn't exist" };
+    });
 }
--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,57 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const clients_1 = require("./clients");
+const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
 const text = "<h1>No mentira</h1>";
+app.use((0, morgan_1.default)('dev'));
 app.get("/", (req, res) => {
     res.send(text);
 });
-app.post("/newClient/:cedula/:nombre", (req, res) => {
-    let result = (0, clients_1.createClient)(req.params.cedula, req.params.nombre);
+app.get('/clients', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let clients = yield (0, clients_1.getClients)();
+    res.status(200).send(clients);
+}));
+app.post("/clients/:cedula/:nombre", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let result = yield (0, clients_1.createClient)(req.params.cedula, req.params.nombre);
     if (result.success) {
         res.status(201).send(result.clients);
     }
     else {
         res.status(400).send(result.message);
     }
-});
+}));
+app.delete('/clients/:cedula', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let result = yield (0, clients_1.deleteClient)(req.params.cedula);
+    if (result.success) {
+        res.status(200).send(result.clients);
+    }
+    else {
+        res.status(400).send(result.message);
+    }
+}));
+app.put('/clients/:cedula/:nombre', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let result = yield (0, clients_1.updateClients)(req.params.cedula, req.params.nombre);
+    if (result.success) {
+        res.status(200).send(result.clients);
+    }
+    else {
+        res.status(400).send(result.message);
+    }
+}));
 app.listen(3000, () => {
     console.log("Server is running on port 3000...");
 });
